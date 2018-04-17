eb95aafa01eaff426f2ef56de7e48e62b72d037f
andremac96 <andremacnamara12@gmail.com>
2018-04-17 17:42:29 +0000

    Chart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-
 //= require bootstrap-sprockets
-//= require_tree 
-//= require highcharts
 //= require Chart.bundle
 //= require chartkick
+//= require_tree
